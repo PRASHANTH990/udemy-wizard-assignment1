3da4bdcf376a316d94a425f77c588a4f632e9c79
prashanth <mailboxofprashanth@gmail.com>
2020-03-24 06:40:55 +0530

    task completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillComponent } from './skill/skill.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'step-1', component: PersonalDetailsComponent},
+  {path: 'step-2', component: ContactDetailsComponent},
+  {path: 'step-3', component: SkillComponent},
+  {path: 'step-4', component: WorkExperienceComponent},
+  {path: '**', redirectTo: 'step-1', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillComponent } from './skill/skill.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalDetailsComponent
+    PersonalDetailsComponent,
+    ContactDetailsComponent,
+    SkillComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule,
